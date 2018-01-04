3779c7ca695f07d2a0c8a43aaf53b8e5aae7629d
Bernie Pope <bjpope@unimelb.edu.au>
2018-01-04 23:19:04 +1100

    Merge overlaping CNVs

--- setup.py
+++ setup.py
@@ -3,27 +3,21 @@
 from distutils.core import setup
 
 LONG_DESCRIPTION = \
-'''The program reads one or more input FASTA files. 
-For each file it computes a variety of statistics, and then
-prints a summary of the statistics as output.
-        
-The goal is to provide a solid foundation for new bioinformatics command line tools,
-and is an ideal starting place for new projects.'''
-
+'''The program'''
 
 setup(
-    name='collapse_cnvs-py',
+    name='collapse_cnvs',
     version='0.1.0.0',
     author='Bernie Pope',
     author_email='bjpope@unimelb.edu.au',
     packages=['collapse_cnvs'],
     package_dir={'collapse_cnvs': 'collapse_cnvs'},
     entry_points={
-        'console_scripts': ['collapse_cnvs-py = collapse_cnvs.collapse_cnvs:main']
+        'console_scripts': ['collapse_cnvs = collapse_cnvs.collapse_cnvs:main']
     },
     url='https://github.com/bjpop/collapse_cnvs',
     license='LICENSE',
     description=('A prototypical bioinformatics command line tool'),
     long_description=(LONG_DESCRIPTION),
-    install_requires=["biopython==1.66"],
+    install_requires=["networkx"],
 )
